5e6fa71db11ec3489b0f47f6bfffe8c6313a4ee2
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2025-03-31 23:49:10 -0400

    initial go setup

--- pipeline/cmd/extract/main.go
+++ pipeline/cmd/extract/main.go
@@ -1,3 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	logFile, err := os.Create("extractor.log")
+	if err != nil {
+		errorMsg := fmt.Sprintf("[ERROR] Could not open log file: %s", err.Error())
+		log.Fatalln(errorMsg)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
+	greetMsg := "[INFO] Starting data extractor..."
+	fmt.Println(greetMsg)
+	log.Println(greetMsg)
+}
